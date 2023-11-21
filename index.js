6b39eb54cca2527794f3efbc8336576879d59ab6
VaibhavBanka <vaibhavbanka12@gmail.com>
2023-11-21 15:04:36 +0530

    f

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const conn=require('./connection/connection')
 require('dotenv').config();
 const path=require('path');
 const User=require('./models/users');
+const List=require("./models/list")
 
 // const __dirname=path.resolve();
 
@@ -14,11 +15,16 @@ const list=require("./routes/list")
 app.use(express.json())
 app.use(cors())
 app.get("/",(req,res)=>{
-    res.status(200).json({message:"Backend is live now"})
+    res.status(200).json({message:"Backend is live now",users:"give: <link>/users",lists:"give: <link>/lists",allTasksofuser: "give:<link>/getTasks/_id"})
 })
 app.get("/users",async (req,res)=>{
     const allusers=await User.find();
     res.status(200).json({data:allusers});
+})
+
+app.get("/lists",async (req,res)=>{
+    const alllists=await List.find();
+    res.status(200).json({data:alllists});
 })
 
 
